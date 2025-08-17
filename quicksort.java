f7e3164c144c5538da572f7640b04e8457adce0c
reddyarchana841-code <reddyarchana841@gmail.com>
2025-08-17 14:20:54 +0530

    program4

---
+++ quicksort.java
@@ -0,0 +1,36 @@
+ class quicksort
+ {
+
+    public static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high], i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {12, 7, 14, 9, 10, 11};
+        quickSort(arr, 0, arr.length - 1);
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
+
